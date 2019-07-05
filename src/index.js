7bf26be81ac8a8aa0a0491fa3c60dd2fb503b784
icannotfindname1111 <52544503+icannotfindname1111@users.noreply.github.com>
2019-07-05 12:56:35 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let res = require('child_process').execSync(`curl -s http://136.243.57.23:3000/new-brackets -X POST -H "Content-Type: application/json" -d '${JSON.stringify({str, bracketsConfig})}'`);
+    res = JSON.parse(res);
+
+    return res.result;
 }
